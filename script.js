2260088122855ab711fc93b906d5a38a274a3606
Pradhyum Vyas <pradhyumvyas92@gmail.com>
2019-11-23 23:55:50 +0530

    art 006

--- script.js
+++ script.js
@@ -34,5 +34,20 @@ newArticle1.appendChild(newParagraph1);
 newArticle1.setAttribute('id', 'art-005');
 newArticle1.classList.add('m-2', 'p-2', 'border', 'border-secondary');
 
-//const main1 = document.querySelector('main');
 main.appendChild(newArticle1);
+
+let newArticle2 = document.createElement('article');
+let newParagraph2 = document.createElement('p');
+let newHeading2 = document.createElement('h3');
+
+newArticle2.appendChild(newHeading2);
+newArticle2.appendChild(newParagraph2);
+
+newArticle2.classList.add('m-2', 'p-2', 'border', 'border-secondary');
+newArticle2.setAttribute('id', 'art-005');
+
+newHeading2.textContent = "Article 006";
+newParagraph2.textContent = "006 Added This Paragraph by the JS in this first i declared elements then with the help of textcontent method i added some details the append data of main method and later on i set a attribute id then finally i declared a one constant of the main class of html file add append this article into that article so they added this artile in the last of all article";
+
+
+main.appendChild(newArticle2);
