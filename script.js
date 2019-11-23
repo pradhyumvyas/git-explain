e4e543e38f3f1fd1ac625bdf21f6eea284d6d62b
Pradhyum Vyas <pradhyumvyas92@gmail.com>
2019-11-23 23:40:57 +0530

    added 5 article

--- script.js
+++ script.js
@@ -19,3 +19,20 @@ newArticle.setAttribute('id', 'art-004');
 
 const main = document.querySelector('main');
 main.appendChild(newArticle);
+
+let newArticle1 = document.createElement('article');
+let newHeading1 = document.createElement('h3');
+let newParagraph1 = document.createElement('p')
+
+newHeading1.textContent = "Article 004";
+newParagraph1.textContent = "Added This Paragraph by the JS in this first i declared elements then with the help of textcontent method i added some details the append data of main method and later on i set a attribute id then finally i declared a one constant of the main class of html file add append this article into that article so they added this artile in the last of all article";
+
+
+newArticle1.appendChild(newHeading1);
+newArticle1.appendChild(newParagraph);
+
+newArticle1.setAttribute('id', 'art-005');
+newArticle1.classList('m-2', 'p-2', 'border', 'border-secondary');
+
+const main1 = document.querySelector('main');
+main1.appendChild(newArticle1);
