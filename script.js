ddf9858d2be08a0123e409700dfa4a1b9f1ffc37
Pradhyum Vyas <pradhyumvyas92@gmail.com>
2019-11-23 23:58:10 +0530

    try

--- script.js
+++ script.js
@@ -20,34 +20,34 @@ newArticle.setAttribute('id', 'art-004');
 const main = document.querySelector('main');
 main.appendChild(newArticle);
 
-let newArticle1 = document.createElement('article');
-let newHeading1 = document.createElement('h3');
-let newParagraph1 = document.createElement('p')
+// let newArticle1 = document.createElement('article');
+// let newHeading1 = document.createElement('h3');
+// let newParagraph1 = document.createElement('p')
 
-newHeading1.textContent = "Article 005";
-newParagraph1.textContent = "Added This Paragraph by the JS in this first i declared elements then with the help of textcontent method i added some details the append data of main method and later on i set a attribute id then finally i declared a one constant of the main class of html file add append this article into that article so they added this artile in the last of all article";
-
-
-newArticle1.appendChild(newHeading1);
-newArticle1.appendChild(newParagraph1);
-
-newArticle1.setAttribute('id', 'art-005');
-newArticle1.classList.add('m-2', 'p-2', 'border', 'border-secondary');
-
-main.appendChild(newArticle1);
-
-let newArticle2 = document.createElement('article');
-let newParagraph2 = document.createElement('p');
-let newHeading2 = document.createElement('h3');
-
-newArticle2.appendChild(newHeading2);
-newArticle2.appendChild(newParagraph2);
-
-newArticle2.classList.add('m-2', 'p-2', 'border', 'border-secondary');
-newArticle2.setAttribute('id', 'art-005');
-
-newHeading2.textContent = "Article 006";
-newParagraph2.textContent = "006 Added This Paragraph by the JS in this first i declared elements then with the help of textcontent method i added some details the append data of main method and later on i set a attribute id then finally i declared a one constant of the main class of html file add append this article into that article so they added this artile in the last of all article";
+newHeading.textContent = "Article 005";
+newParagraph.textContent = "Added This Paragraph by the JS in this first i declared elements then with the help of textcontent method i added some details the append data of main method and later on i set a attribute id then finally i declared a one constant of the main class of html file add append this article into that article so they added this artile in the last of all article";
 
 
-main.appendChild(newArticle2);
+newArticle.appendChild(newHeading);
+newArticle.appendChild(newParagraph);
+
+// newArticle1.setAttribute('id', 'art-005');
+// newArticle1.classList.add('m-2', 'p-2', 'border', 'border-secondary');
+
+main.appendChild(newArticle);
+
+// let newArticle2 = document.createElement('article');
+// let newParagraph2 = document.createElement('p');
+// let newHeading2 = document.createElement('h3');
+
+// newArticle2.appendChild(newHeading2);
+// newArticle2.appendChild(newParagraph2);
+
+// newArticle2.classList.add('m-2', 'p-2', 'border', 'border-secondary');
+// newArticle2.setAttribute('id', 'art-005');
+
+// newHeading2.textContent = "Article 006";
+// newParagraph2.textContent = "006 Added This Paragraph by the JS in this first i declared elements then with the help of textcontent method i added some details the append data of main method and later on i set a attribute id then finally i declared a one constant of the main class of html file add append this article into that article so they added this artile in the last of all article";
+
+
+// main.appendChild(newArticle2);
